d4d89f724817d5f37ae399c84782b49b2a6efd5c
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2026-04-27 10:21:37 +0100

    added full stuff

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 app.use(cors())
 app.use(express.json());
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('./connection');
+
+const serverPort = process.env.PORT || 5643
 
 mongoose.connect(process.env.URL)
     .then(() => console.log('Connected Successfully to MongoDB'))
     .catch(err => console.error('Connection Failed', err));
 
 
-const serverPort = process.env.PORT || 5643
-
 app.get('/', (req, res) => {
     res.json({ 'message': "I am running fine" })
+})
+
+app.get('/test', async (req, res) => {
+    const Test = mongoose.model('Test', new mongoose.Schema({ name: String }));
+    await Test.create({ name: 'hello todo2026' });
+    res.json({ message: 'saved Successfully!' });
 })
 
 app.listen(serverPort, () => {
